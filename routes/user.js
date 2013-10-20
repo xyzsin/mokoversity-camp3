a30c4abaf6b94918a69fe255cfb7a3753a356c3f
xyzsin <xyzsin2@gamil.com>
2013-10-20 14:46:59 +0800

    fix #15

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@
 /*
  * GET users listing.
  */
-
+var scores = [];
 
 exports.list = function(req, res){
   res.send("respond with a resource");
@@ -10,6 +10,6 @@ exports.list = function(req, res){
 
 
 exports.save = function(req, res){
-  
-   console.log("Your scores =" + req.url);
+  scores.push(req.query.scores);
+   console.log("Your scores =" + scores);
 };
